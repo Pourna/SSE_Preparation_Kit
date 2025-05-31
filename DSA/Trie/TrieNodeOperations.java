abd6e2ee1e9300d70844d52baf07c2814353901e
rpourna <rpourna@vmware.com>
2025-05-31 12:49:52 +0530

    Tries related problem

---
+++ DSA/Trie/TrieNodeOperations.java
@@ -0,0 +1,68 @@
+/*
+*Time Complexity
+*
+insert(word) → O(L)
+search(word) → O(L)
+startsWith(prefix) → O(L)
+*
+Where L = length of the word or prefix
+*/
+
+public class TrieNodeOperations {
+
+    static class Trie {
+        static class TrieNode {
+            TrieNode[] children;
+            boolean isWordEnded;
+
+            TrieNode() {
+                children=new TrieNode[26];
+                isWordEnded = false;
+            }
+        }
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for(char c : word.toCharArray()) {
+                if (node.children[c-'a']==null) node.children[c-'a'] = new TrieNode();
+                node = node.children[c-'a'];
+            }
+            node.isWordEnded=true;
+        }
+
+        public boolean search(String word) {
+            TrieNode node = root;
+            for(char c : word.toCharArray()) {
+                if (node.children[c-'a'] == null) return false;
+                node = node.children[c-'a'];
+            }
+            return node.isWordEnded;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for(char c : prefix.toCharArray()) {
+                if(node.children[c-'a'] == null) return false;
+                node = node.children[c-'a'];
+            }
+            return node!=null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        trie.insert("banana");
+        System.out.println("Search for apple: "+ trie.search("apple"));
+        System.out.println("Start with ban: "+ trie.startsWith("ban"));
+        System.out.println("Search for app: "+ trie.search("app"));
+        trie.insert("app");
+        System.out.println("Search for app: "+ trie.search("app"));
+    }
+}
--- DSA/Trie/TriesREADME.md
+++ DSA/Trie/TriesREADME.md
@@ -1,6 +1,7 @@
 #### Tries Related DSA Problems
 
-| S.No | Problem                   | Solution                         | Explanation                        |  
-|------|---------------------------|----------------------------------|------------------------------------|
-| 1.   | Search Suggestions System | [Java Code](https://example.com) | [Explanation](https://example.com) |
+| S.No | Problem                   | Solution                                                                                                  | Explanation                                                                                        |  
+|------|---------------------------|-----------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------------------------------|
+| 1.   | Search Suggestions System | [Java Code](https://github.com/Pourna/SSE_Preparation_Kit/blob/main/DSA/Trie/SearchSuggestionEngine.java) | [Explanation](https://github.com/Pourna/SSE_Preparation_Kit/blob/main/DSA/Trie/SearchEngineDoc.md) | 
+| 2.   | TriesNode Operations      | [Java Code](https://github.com/Pourna/SSE_Preparation_Kit/blob/main/DSA/Trie/TrieNodeOperations.java) | [Explanation](https://github.com/Pourna/SSE_Preparation_Kit/blob/main/DSA/Trie/TrieNodeOperations.md)                                                                                                   |
 
